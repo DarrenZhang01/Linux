8575b638b3d7980ba20158c31fd14d72548fc38d
Darren <18633059886@163.com>
2018-08-27 16:54:20 +0800

    Add a new showcase shell file fot git commands

---
+++ git/showcase.sh
@@ -0,0 +1,8 @@
+# This file contains a simple show case of the git command
+# To execute this file, type in the command line:
+#     shell showcase.sh
+git clone https://github.com/DarrenZhang01/Linux.git
+cd Linux
+git status
+cd ..
+rm -rf Linux
